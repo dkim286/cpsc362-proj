d3d7f43bd4d3b6271f04cd98763b0a421e46c0a7
dkim286 <dkim286@csu.fullerton.edu>
2021-06-08 18:37:25 -0700

    Initial implementation of colors.py

--- ttt/colors.py
+++ ttt/colors.py
@@ -1,11 +1,26 @@
 '''
 Provides the RGB color-tuples used by the game for drawing things in the GUI.
+
+Format:
+    (red, green, blue)
 '''
 
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (128, 128, 128)
+SILVER = (192, 192, 192)
 
-Color = tuple[int, int, int]
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
 
-def color_to_str(c: Color) -> str:
-    return '(Red, {}; Green: {}, Blue: {})'.format(c[0], c[1], c[2])
+DARK_RED = (128, 0, 0)
+DARK_YELLOW = (128, 128, 0)
+DARK_GREEN = (0, 128, 0)
+DARK_BLUE = (0, 0, 128)
+PURPLE = (128, 0, 128)
+TEAL = (0, 128, 128)
