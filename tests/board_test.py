9a50ccd6fd36d115f1be579550e527b39f9250fe
Kavya Vandana Jayarama Reddy <kavyavandanajayaramareddy@Kavyas-MacBook-Air.local>
2021-06-10 09:44:58 +0530

    Adding unit test scnearios and func prototypes.

--- tests/board_test.py
+++ tests/board_test.py
@@ -9,3 +9,17 @@ def test_board_drawToken():
     y = board.drawToken('y')
     assert x == y
 
+# test board columns.
+def test_board_columns():
+    board = Board()
+    pass
+
+# test the diagonals of the board.
+def test_board_diagonals():
+    board = Board()
+    pass
+
+# test the rows of the board.
+def test_board_rows():
+    board = Board()
+    pass
--- tests/gametest.py
+++ tests/gametest.py
@@ -1,3 +1,30 @@
-'''
-Test file for testing the Game object.
-'''
+import pytest
+import pygame as pg, sys
+from ttt.colors import WHITE
+from ttt.dimensions import BOARD_WIDTH as width, BOARD_HEIGHT as height, TTT as board
+from ttt.board import Board
+
+# test the game status.
+def test_update_board():
+    board = Board()
+    pass
+
+# test the column win scenario.
+def test_check_column_win(self):
+    board = Board()
+    pass
+
+# test the row win scenario.
+def test_check_row_win(self):
+    board = Board()
+    pass
+
+# test the diagonal win scenario.
+def test_check_first_diagonal_win(self):
+    board = Board()
+    pass
+
+# test the second diagonal win scenario.
+def test_check_second_diagonal_win(self):
+    board = Board()
+    pass
