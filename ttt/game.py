7c0108a61be4cf65a312fb89de981b089b38e7ae
davidqdo <47126935+davidqdo@users.noreply.github.com>
2021-06-09 20:02:27 -0700

    Update game.py
    Replaced some functions

--- ttt/game.py
+++ ttt/game.py
@@ -26,11 +26,7 @@ class Game:
             status = 'Tie Game'
         font = pg.font.Font(None, 30)
         text = font.render(status, 1, WHITE)
-        # copy the rendered message onto the board
-        self.screen.fill ((0, 0, 0), (0, 400, 500, 100))
-        text_rect = text.get_rect(center=(width/2, 500-50))
-        self.screen.blit(text, text_rect)
-        pg.display.update()
+        renderBoard(self)
         
     def win_checker(self):
         global winner,draw
@@ -39,27 +35,27 @@ class Game:
             if ((board[row][0] == board[row][1] == board[row][2]) and(board[row][0] is not None)):
                 # this row won
                 winner = board[row][0]
-                pg.draw.line(self.screen, (250,0,0), (0, (row + 1)*height/3 -height/6),\
-                                (width, (row + 1)*height/3 - height/6 ), 4)
+                drawRowLine(self, row)
                 break
+                
         # check for winning columns
         for col in range (0, 3):
             if (board[0][col] == board[1][col] == board[2][col]) and (board[0][col] is not None):
                 # this column won
                 winner = board[0][col]
                 #draw winning line
-                pg.draw.line (self.screen, (250,0,0),((col + 1)* width/3 - width/6, 0),\
-                            ((col + 1)* width/3 - width/6, height), 4)
+                drawColumnLine(self, column)
                 break
+                
         # check for diagonal winners
         if (board[0][0] == board[1][1] == board[2][2]) and (board[0][0] is not None):
             # game won diagonally left to right
             winner = board[0][0]
-            pg.draw.line (self.screen, (250,70,70), (50, 50), (350, 350), 4)
+            drawDiagonalLTR(self)
         if (board[0][2] == board[1][1] == board[2][0]) and (board[0][2] is not None):
             # game won diagonally right to left
             winner = board[0][2]
-            pg.draw.line (self.screen, (250,70,70), (350, 50), (50, 350), 4)
+            drawDiagonalRTL(self)
         if(all([all(row) for row in board]) and winner is None ):
             draw = True
         draw_game_status(self)
