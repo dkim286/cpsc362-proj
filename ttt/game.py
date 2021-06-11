ad702200da1916a5b4b7be75c782d9227a2dd36f
dkim286 <dkim286@csu.fullerton.edu>
2021-06-10 21:15:41 -0700

    renamed the instance variable _XO to something that's more representative

--- ttt/game.py
+++ ttt/game.py
@@ -8,6 +8,9 @@ import time
 DRAW = 'D'
 UNDECIDED = 'N'
 
+X = X
+O = O
+
 class Game:
     '''
     A class that represents the game's state.
@@ -18,7 +21,7 @@ class Game:
     
     # Game Constructor
     def __init__(self):
-        self._XO = 'x'
+        self._player = X
         self._board = [[None]*3,[None]*3,[None]*3]
     
         
@@ -28,18 +31,18 @@ class Game:
         # check for winning rows
         for row in range (0,3):
             if ((board[row][0] == board[row][1] == board[row][2]) and(board[row][0] is not None)):
-                return self._XO
+                return self._player
                 
         # check for winning columns
         for col in range (0, 3):
             if (board[0][col] == board[1][col] == board[2][col]) and (board[0][col] is not None):
-                return self._XO
+                return self._player
                 
         # check for diagonal winners
         if (board[0][0] == board[1][1] == board[2][2]) and (board[0][0] is not None):
-            return self._XO
+            return self._player
         if (board[0][2] == board[1][1] == board[2][0]) and (board[0][2] is not None):
-            return self._XO
+            return self._player
 
         if(all([all(row) for row in board])):
             return DRAW
@@ -49,13 +52,13 @@ class Game:
     # Helper util to reset the game state.
     def reset_game(self):
         time.sleep(3)
-        self._XO = 'x'
+        self._player = X
         self._board = [[None]*3,[None]*3,[None]*3]
     
-    # Helper util to place an 'x' or 'o' onto the chosen space within the board
+    # Helper util to place an X or O onto the chosen space within the board
     def place_move(self, row: int, col: int) -> bool:
         if self._board[row-1][col-1] == None:
-            self._board[row-1][col-1] = self._XO
+            self._board[row-1][col-1] = self._player
             self._change_player()
             return True
         else:
@@ -63,7 +66,7 @@ class Game:
             
     # Helper util to change the current active player
     def _change_player(self) -> None:
-        if self._XO == 'x':
-            self._XO = 'o'
+        if self._player == X:
+            self._player = O
         else:
-            self._XO = 'x'
+            self._player = X
