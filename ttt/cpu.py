ff86222df94f4343e7fe3cab51e0be44f48e5af3
kavyavandana <kavyakothvari@gmail.com>
2021-06-13 23:49:39 +0000

    adding a high level prototype

--- ttt/cpu.py
+++ ttt/cpu.py
@@ -1,3 +1,21 @@
-'''
-Provides functions and other tools for the computer player logic. 
-'''
+import pygame as pg, sys
+from ttt.dimensions import *
+
+class Cpu:
+    '''
+    Provides functions and other tools for the computer player logic.
+
+    '''
+
+    # CPU Constructor
+    def __init__(self, x : int, y : int):
+        self._move = (x,y)
+
+    def find_best_move(self, depth: int) :
+        pass
+
+    def _min_max(self):
+        pass
+
+    def _evaluate_game(self):
+        pass
