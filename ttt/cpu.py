d0530748968f12918623764a42f3232fccd58aff
kavyavandana <kavyakothvari@gmail.com>
2021-06-14 00:12:36 +0000

    added just the base func prototypes for cpu

--- ttt/cpu.py
+++ ttt/cpu.py
@@ -1,6 +1,9 @@
 import pygame as pg, sys
 from ttt.dimensions import *
 
+# custom type that shows the x and y coordinates of the cell (x,y).
+_move = (int, int)
+
 class Cpu:
     '''
     Provides functions and other tools for the computer player logic.
@@ -11,11 +14,14 @@ class Cpu:
     def __init__(self, x : int, y : int):
         self._move = (x,y)
 
-    def find_best_move(self, depth: int) :
+    # helper util to find the next possible move for the cpu player.
+    def find_best_move(self, depth: int) -> _move :
         pass
 
-    def _min_max(self):
+    # helper util to find the next cell for the game.
+    def _min_max(self) -> int:
         pass
 
-    def _evaluate_game(self):
+    # helper util to evaluate the game state.
+    def _evaluate_game(self) -> int:
         pass
