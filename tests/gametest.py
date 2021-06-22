ffe45648bfa8ceab1cd5e1ce5a197e603eb7bc34
kavyavandana <kavyakothvari@gmail.com>
2021-06-22 23:04:17 +0000

    Adding some additional tests

--- tests/gametest.py
+++ tests/gametest.py
@@ -5,27 +5,56 @@ from ttt.dimensions import BOARD_WIDTH as width, BOARD_HEIGHT as height, TTT as 
 from ttt.board import Board
 from ttt.game import Game
 
-# test the game status.
-def test_update_board():
-    board = Board()
 
+def test_update_board() -> None:
+    '''
+    Test the update board action in the game board.
 
-# test the column win scenario.
-def test_check_column_win(self):
+    '''
+    board = [[None, None, None], [None, None, None], [None, None, None]]
+    pass
+
+def test_check_column_win() -> None:
+    '''
+    Test the column win scenario in the game board.
+
+    '''
     board = [['O', 'O', 'O'], [None, None, None], [None, None, None]]
-    pass
+    game = Game()
+    game._board = board
+    winner, direction, value = game.win_checker()
+    assert winner == 'o'
 
-# test the row win scenario.
-def test_check_row_win(self):
+def test_check_row_win() -> None:
+    '''
+    Test the row win scenario in the game board.
+
+    '''
     board = [['X', None, None], ['X', None, None], ['X', None, None]]
-    pass
+    game = Game()
+    game._board = board
+    winner, direction, value = game.win_checker()
+    assert winner == 'y'
 
-# test the diagonal win scenario.
-def test_check_first_diagonal_win(self):
+def test_check_first_diagonal_win() -> None:
+    '''
+    Test the first diagonal win scenario in the game board.
+
+    '''
     board = [['X', None, None], [ None, 'X', None], [None, None, 'X']]
-    pass
+    game = Game()
+    game._board = board
+    winner, direction, value = game.win_checker()
+    assert winner == 'x'
 
-# test the second diagonal win scenario.
-def test_check_second_diagonal_win(self):
+def test_check_second_diagonal_win() -> None:
+    '''
+    Test the second diagonal win scenario in the game board.
+
+    '''
     board = [[None, None, 'O'], [None, 'O', None], ['O', None, None]]
-    pass
+    game = Game()
+    game._board = board
+    winner, direction, value = game.win_checker()
+    assert winner == 'o'
+
